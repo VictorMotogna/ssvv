f07bdf2cb7df130a564adc210a245f1fec83961d
Victor Motogna <vmotogna@gmail.com>
2018-03-14 10:57:58 +0200

    added project files

--- ssvvlab2/src/main/java/ssvvlab2/App.java
+++ ssvvlab2/src/main/java/ssvvlab2/App.java
@@ -1,13 +1,18 @@
 package ssvvlab2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ssvvlab2.repository.Repository;
+import ssvvlab2.ui.DoctorUI;
+import ssvvlab2.controller.DoctorController;
+
+public class App {
+
+    public static void main(String[] args) {
+        String patients = "src/main/FilePatients.txt";
+        String consultations = "src/main/FileConsultations.txt";
+        Repository repo = new Repository(patients, consultations);
+        DoctorController ctrl = new DoctorController(repo);
+
+        DoctorUI console = new DoctorUI(ctrl);
+        console.Run();
     }
 }
