c52e52fca5e571bbfa30371f8af30e1c00f07a81
Victor Motogna <vmotogna@gmail.com>
2018-03-14 11:31:22 +0200

    refactoring structure

---
+++ ssvvlab2/src/main/java/App.java
@@ -0,0 +1,16 @@
+import repository.Repository;
+import ui.DoctorUI;
+import controller.DoctorController;
+
+public class App {
+
+    public static void main(String[] args) {
+        String patients = "src/main/FilePatients.txt";
+        String consultations = "src/main/FileConsultations.txt";
+        Repository repo = new Repository(patients, consultations);
+        DoctorController ctrl = new DoctorController(repo);
+
+        DoctorUI console = new DoctorUI(ctrl);
+        console.Run();
+    }
+}
---
+++ ssvvlab2/src/test/java/AppTest.java
@@ -0,0 +1,67 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import controller.DoctorController;
+import exceptions.PatientException;
+import repository.Repository;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+    private Repository rep;
+    private DoctorController ctrl;
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    /**
+     * Testing patient number
+     */
+    public void test_PatientsNumber() {
+        rep = new Repository("src/test/java/FilePatientsTests.txt", "src/test/java/FileConsultationsTests.txt");
+
+        ctrl = new DoctorController(rep);
+        int noPatients = ctrl.getPatientList().size();
+
+        assertEquals(2, noPatients);
+    }
+
+    /**
+     * Testing patients with disease sore throat number
+     */
+    public void test_PatientsWithDiseaseNumber() {
+        rep = new Repository("src/test/java/FilePatientsTests.txt", "src/test/java/FileConsultationsTests.txt");
+
+        ctrl = new DoctorController(rep);
+        int noPatients = 0;
+        try {
+            noPatients = ctrl.getPatientsWithDisease("sore throat").size();
+        } catch (PatientException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(1, noPatients);
+    }
+}
