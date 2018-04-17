899aba886e6a77c24f0283c294ceac196970be3c
Victor Motogna <vmotogna@gmail.com>
2018-04-17 20:31:16 +0300

    wbt lab3

--- src/test/java/AppTestWBT.java
+++ src/test/java/AppTestWBT.java
@@ -1,4 +1,5 @@
 import controller.DoctorController;
+import exceptions.PatientException;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -49,6 +50,73 @@ public class AppTestWBT extends TestCase {
         int numberOfConsultations = controller.getConsultationList().size();
 
         assertEquals(4, numberOfConsultations);
+        System.out.println();
+    }
+
+    public void testPatientsWithDiseaseLoop1() {
+        repository = new Repository("src/test/java/FilePatientsLoop1.txt", "src/test/java/FileConsultationsLoop1.txt");
+        controller =  new DoctorController(repository);
+
+        int numberOfConsultations = 0;
+        try {
+            numberOfConsultations = controller.getPatientsWithDisease("sore throat").size();
+        } catch (PatientException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(1, numberOfConsultations);
+        System.out.println();
+    }
+
+    public void testPatientsWithDiseaseLoop2() {
+        repository = new Repository("src/test/java/FilePatientsLoop2.txt", "src/test/java/FileConsultationsLoop2.txt");
+        controller =  new DoctorController(repository);
+
+        int numberOfConsultations = 0;
+        try {
+            numberOfConsultations = controller.getPatientsWithDisease("sore throat").size();
+        } catch (PatientException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(2, numberOfConsultations);
+        System.out.println();
+    }
+
+    public void testAllPatientsPath0() {
+        repository = new Repository("src/test/java/FilePatientsAllPath0.txt", "src/test/java/FileConsultationsAllPath0.txt");
+        controller =  new DoctorController(repository);
+
+        int numberOfPatients = controller.getPatientList().size();
+
+        assertEquals(0, numberOfPatients);
+        System.out.println();
+    }
+
+    public void testAllConsultationsPath0() {
+        repository = new Repository("src/test/java/FilePatientsAllPath0.txt", "src/test/java/FileConsultationsAllPath0.txt");
+        controller =  new DoctorController(repository);
+
+        int numberOfConsultations = controller.getConsultationList().size();
+
+        assertEquals(0, numberOfConsultations);
+        System.out.println();
+    }
+
+    public void testPatientsDiseaseTrue() {
+        repository = new Repository("src/test/java/FilePatientsTrue.txt", "src/test/java/FileConsultationsTrue.txt");
+        controller =  new DoctorController(repository);
+
+        int numberOfConsultations = 0;
+        try {
+            numberOfConsultations = controller.getPatientsWithDisease("sore throat").size();
+        } catch (PatientException e) {
+            e.printStackTrace();
+        }
+
+        boolean result = (numberOfConsultations == 1);
+
+        assertEquals(true, result);
         System.out.println();
     }
 }
